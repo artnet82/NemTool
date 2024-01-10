a5da3d1b50d857e6f4be6670fa1f739c33d1b714
Н Алексей <108236913+artnet82@users.noreply.github.com>
2024-01-10 17:37:51 +0300

    Create NemToolAPI.py
    В этом примере класс NemToolAPI представляет API для работы с PostgreSQL базой данных с использованием библиотеки psycopg2. Конструктор класса инициализирует подключение к базе данных с заданными параметрами хоста, порта, имени базы данных, имени пользователя и пароля.
    
    API включает следующие методы:
    
        create_table(table_name, columns): Создает таблицу с указанным именем и столбцами.
        insert_data(table_name, data): Вставляет данные в указанную таблицу.
        select_data(table_name, condition=None): Выполняет выборку данных из указанной таблицы с опциональным условием.
        close_connection(): Закрывает соединение с базой данных.
    
    Вы можете использовать этот пример API в своем проекте, создав экземпляр класса NemToolAPI с соответствующими параметрами подключения к вашей PostgreSQL базе данных и вызывая соответствующие методы для выполнения операций с базой данных.
    
    Обратите внимание, что для использования этого кода вам потребуется установить библиотеку psycopg2. Вы можете установить ее с помощью pip командой pip install psycopg2.

---
+++ NemToolAPI.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+class NemToolAPI:
+    def __init__(self, host, port, database, user, password):
+        self.connection = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+        self.cursor = self.connection.cursor()
+
+    def create_table(self, table_name, columns):
+        query = f"CREATE TABLE {table_name} ({', '.join(columns)})"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def insert_data(self, table_name, data):
+        columns = ', '.join(data.keys())
+        values = ', '.join([f"'{value}'" for value in data.values()])
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def select_data(self, table_name, condition=None):
+        query = f"SELECT * FROM {table_name}"
+        if condition:
+            query += f" WHERE {condition}"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
